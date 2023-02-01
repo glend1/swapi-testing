779990d754beb3e5be1d5607eff6cb8e8f763764
Glen <glen_dovey@hotmail.com>
2023-02-01 16:32:58 +0000

    implemented fetch for swapi

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [name, setName] = useState()
+    useEffect(() => {
+        fetch('https://swapi.dev/api/people/1/?format=json')
+            .then((res) => res.json())
+            .then((json) => setName(json.name))
+    }, [])
+
+    return <h1>{name}</h1>
 }
 
-export default App;
+export default App
